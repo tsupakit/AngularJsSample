7ae5fe984665b9dc1aa0f815d434289d8661b748
Supakit Thanomboonchareon <supakitt@outlook.com>
2014-05-21 18:30:37 +0700

    Update views and controllers

--- AngularJsSample/Controllers/DocumentsController.cs
+++ AngularJsSample/Controllers/DocumentsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -8,60 +9,73 @@ using System.Web.Http;
 
 namespace Draycir.DM.Administration.Web.Controllers
 {
-    [RoutePrefix("api/documents")]
+    //[RoutePrefix("api/documents")]
     public class DocumentsController : ApiController
     {
-        private static List<DeletedDocument> _deletedDocuments;
+        private const int _max = 20;
+        //private static List<DeletedDocument> _deletedDocuments;
+
+        private List<DeletedDocument> Mock
+        {
+            get
+            {
+                var data = new List<DeletedDocument>();
+
+                for (int i = 1; i <= _max; i++)
+                {
+                    var prime = IsPrime(i);
+                    DateTime? protectionStart = null;
+                    DateTime? protectionEnd = null;
+
+                    if (prime)
+                    {
+                        protectionStart = DateTime.Now;
+                        protectionEnd = protectionStart.Value.AddMonths(1);
+                    }
+
+                    data.Add(new DeletedDocument
+                    {
+                        DocumentId = Guid.NewGuid(),
+                        DocumentName = "Test" + i,
+                        DocumentType = "Transaction" + i,
+                        DocumentDate = DateTime.Now.AddDays(i - 1),
+                        ArchivedBy = "Supakit.T",
+                        DeletedDate = DateTime.Now,
+                        DeletedBy = "Supakit.T",
+                        IsProtected = prime,
+                        ProtectionStartDate = protectionStart,
+                        ProtectionEndDate = protectionEnd
+                    });
+                }
+
+                return data;
+            }
+        }
+
+        private bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+            if (number == 2)
+                return true;
+            if ((number & 1) == 0)
+                return false;
+
+            for (int i = 3; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
 
         private List<DeletedDocument> DeletedDocuments
         {
             get
             {
-                return _deletedDocuments ?? (_deletedDocuments = new List<DeletedDocument> { new DeletedDocument {
-                    DocumentId = Guid.NewGuid(),
-                    DocumentName = "Test1",
-                    DocumentType = "Image",
-                    DocumentDate = DateTime.Now.AddDays(-1),
-                    ArchivedBy = "Supakit.T",
-                    DeletedDate = DateTime.Now,
-                    DeletedBy = "Supakit.T",
-                    IsProtected = false,
-                    ProtectionStartDate = DateTime.Now.AddMonths(-1),
-                    ProtectionEndDate = DateTime.Now.AddMonths(1)
-                }, new DeletedDocument {
-                    DocumentId = Guid.NewGuid(),
-                    DocumentName = "Test2",
-                    DocumentType = "Transaction",
-                    DocumentDate = DateTime.Now.AddDays(-1),
-                    ArchivedBy = "Supakit.T",
-                    DeletedDate = DateTime.Now,
-                    DeletedBy = "Supakit.T",
-                    IsProtected = true,
-                    ProtectionStartDate = DateTime.Now.AddMonths(-1),
-                    ProtectionEndDate = DateTime.Now.AddMonths(1)
-                }, new DeletedDocument {
-                    DocumentId = Guid.NewGuid(),
-                    DocumentName = "Test3",
-                    DocumentType = "Transaction",
-                    DocumentDate = DateTime.Now.AddDays(-1),
-                    ArchivedBy = "Supakit.T",
-                    DeletedDate = DateTime.Now,
-                    DeletedBy = "Supakit.T",
-                    IsProtected = true,
-                    ProtectionStartDate = DateTime.Now.AddMonths(-1),
-                    ProtectionEndDate = DateTime.Now.AddMonths(1)
-                }, new DeletedDocument {
-                    DocumentId = Guid.NewGuid(),
-                    DocumentName = "Test4",
-                    DocumentType = "Transaction",
-                    DocumentDate = DateTime.Now.AddDays(-1),
-                    ArchivedBy = "Supakit.T",
-                    DeletedDate = DateTime.Now,
-                    DeletedBy = "Supakit.T",
-                    IsProtected = false,
-                    ProtectionStartDate = DateTime.Now.AddMonths(-1),
-                    ProtectionEndDate = DateTime.Now.AddMonths(1)
-                }});
+                //return _deletedDocuments ?? (_deletedDocuments = Mock);
+                return Mock;
             }
         }
 
@@ -88,34 +102,58 @@ namespace Draycir.DM.Administration.Web.Controllers
         }
 
         //default http get verb
-        public IHttpActionResult GetDeletedDocuments()
+        [HttpGet]
+        public IEnumerable<DeletedDocument> GetDeletedDocuments()
         {
             //IEnumerable<DeletedDocumentResult> deletedDocuments = _adminProxy.GetDeletedDocuments();
             List<DeletedDocument> deletedDocuments = DeletedDocuments;
-            return Ok(deletedDocuments);
+            return deletedDocuments;
         }
 
-        [Route("purge")]
+        //public HttpResponseMessage GetFile(string id)
+        //{
+        //    if (String.IsNullOrEmpty(id))
+        //        return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+        //    string fileName = "test";
+        //    string localFilePath;
+        //    //int fileSize;
+
+        //    localFilePath = System.Web.Hosting.HostingEnvironment.MapPath(@"/Files/img.png"); //getFileFromID(id, out fileName, out fileSize);
+
+        //    HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+        //    response.Content = new StreamContent(new FileStream(localFilePath, FileMode.Open, FileAccess.Read));
+        //    //response.Content = new ByteArrayContent(new FileStream(localFilePath, FileMode.Open, FileAccess.Read));
+        //    response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/png");
+        //    response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+        //    response.Content.Headers.ContentDisposition.FileName = fileName;
+
+        //    return response;
+        //}
+
+        //[Route("purge")]
         [HttpPost]
-        public IHttpActionResult PurgeDocuments(string[] id)
+        [ActionName("purge")]
+        public int PurgeDocuments(string[] id)
         {
             //DeletedDocuments.RemoveAll(x => id.Contains(x.DocumentName));
             Guid[] documentIds = Convert(id);
 
-            int result = documentIds.Length; // _adminProxy.PurgeDocuments(documentIds);
+            int result = documentIds.Length; //_adminProxy.PurgeDocuments(documentIds); 
 
-            return Ok(result);
+            return result;
         }
 
-        [Route("restore")]
+        //[Route("restore")]
         [HttpPost]
-        public IHttpActionResult RestoreDocuments(string[] id)
+        [ActionName("restore")]
+        public int RestoreDocuments(string[] id)
         {
             Guid[] documentIds = Convert(id);
 
             //_adminProxy.RestoreDocuments(documentIds);
 
-            return Ok(documentIds.Length);
+            return documentIds.Length;
         }
 
     }
@@ -130,15 +168,7 @@ namespace Draycir.DM.Administration.Web.Controllers
         public DateTime DeletedDate { get; set; }
         public string DeletedBy { get; set; }
         public bool IsProtected { get; set; }
-        public DateTime ProtectionStartDate { get; set; }
-        public DateTime ProtectionEndDate { get; set; }
-
-        public bool IsProtecting
-        {
-            get
-            {
-                return IsProtected && ProtectionEndDate >= DateTime.UtcNow;
-            }
-        }
+        public DateTime? ProtectionStartDate { get; set; }
+        public DateTime? ProtectionEndDate { get; set; }
     }
 }
--- AngularJsSample/app/controllers/documentController.js
+++ AngularJsSample/app/controllers/documentController.js
@@ -1,98 +1,131 @@
-﻿app.controller('documentController', function ($scope, documentService, toaster, dialog) {
+﻿app.controller('documentController', function ($scope, documentService, dialog) {
 
     $scope.deletedDocuments = [];
-    $scope.selectedDocs = [];    
+    $scope.selectedDocs = [];
 
     init();
 
-    function init()
-    {
+    function init() {
         getDeletedDocuments();
     }
 
-    function getDeletedDocuments()
-    {
+    function getDeletedDocuments() {
         documentService.getDeletedDocuments().then
         (
-            function (results)
-            {
+            function (results) {
                 $scope.deletedDocuments = results.data;
                 $scope.gridOptions.selectAll(false);
             },
-            function (error)
-            {
+            function (error) {
                 alert(error.message);
             }
         );
     }
 
-    function getSelectedIds()
-    {
+    function getSelectedIds() {
         var selectedIds = [];
 
-        for (var index in $scope.selectedDocs)
-        {
+        for (var index in $scope.selectedDocs) {
             selectedIds.push($scope.selectedDocs[index].DocumentId);
         }
 
         return selectedIds;
     }
 
-    $scope.canRestore = function ()
-    {
+    $scope.canRestore = function () {
         return $scope.selectedDocs.length > 0;
     }
 
-    $scope.canPurge = function () 
-    {
+    $scope.canPurge = function () {
         return $scope.selectedDocs.length > 0;
     }
 
-    $scope.delete = function ()
-    {
-        if (Enumerable.From($scope.selectedDocs).All(function (doc) { return doc.IsProtecting }))
-        {
+    $scope.delete = function () {
+        if (Enumerable.From($scope.selectedDocs).All(function (doc) { return doc.IsProtected })) {
             dialog.show("The documents selected can not be deleted as they are protected and are still within the protection period.", "Unable to delete", alertView);
             return;
         }
 
         var confirmMessage = 'Are you sure you want to permanently delete the selected document(s)? Once deleted, these can not be restored.';
 
-        if (Enumerable.From($scope.selectedDocs).Any(function (doc) { return doc.IsProtecting }))
-        {
+        if (Enumerable.From($scope.selectedDocs).Any(function (doc) { return doc.IsProtected })) {
             confirmMessage = 'At least one of the documents selected is a protected document and is still within the protection period therefore can not be deleted. Would you like to continue to permanently delete the other document(s)? Once deleted, these can not be restored.';
         }
 
         dialog.show(confirmMessage, "Permanently Delete", confirmView).then(function () {
             var selectedIds = Enumerable.From($scope.selectedDocs)
-                                        .Where(function (doc) { return !doc.IsProtecting })
+                                        .Where(function (doc) { return !doc.IsProtected })
                                         .Select(function (doc) { return doc.DocumentId }).ToArray(); //getSelectedIds();
 
             documentService.deleteDocuments(selectedIds).then(getDeletedDocuments);
         });
-        
+
     };
 
-    $scope.restore = function ()
-    {
-        var confirmMessage = 'Are you sure you want to restore the selected document(s)? This action will re-archive the document(s) and make them available in your host system.';
+    $scope.restore = function () {
+        var confirmMessage = 'Are you sure you want to restore the selected document(s)? This action will re-archive the document(s) and make them available for viewing.';
 
         dialog.show(confirmMessage, "Restore Document", confirmView).then(function () {
             var selectedIds = getSelectedIds();
 
             documentService.restoreDocument(selectedIds).then(getDeletedDocuments);
         });
-        
+
     };
 
     //------ ngGrid section ------//
+    $scope.documentType = 'All';
+    $scope.filterText = '';
+    $scope.selectAll = false;
+
+    //$scope.pagingOptions = {
+    //    pageSizes: [5, 10, 20],
+    //    pageSize: 5,
+    //    currentPage: 1
+    //};
+
+    $scope.$watch('selectAll', function (selectedValue) {
+        $scope.gridOptions.selectVisible(selectedValue);
+    });
+
+    //$scope.$watch('documentType', function (selectedType) {
+    //    var filteredRows = $scope.deletedDocuments;
+
+    //    var result = [];
+
+    //    if (selectedType == 'All') {
+    //        result = $scope.deletedDocuments;
+    //    }
+    //    else if (selectedType == 'Unprotected') {
+    //        result = Enumerable.From(filteredRows).Where(function (doc) { return !doc.IsProtected }).ToArray();            
+    //    }
+    //    else if (selectedType == 'Protected') {
+    //        result = Enumerable.From(filteredRows).Where(function (doc) { return doc.IsProtected }).ToArray();
+    //    }
+
+    //    $scope.gridOptions.data = result;
+    //});
+
+    $scope.$watch('filterText', function (newText) {
+        $scope.gridOptions.filterOptions.filterText = newText;
+        //myplugin.grid.searchProvider.evalFilter();
+    });
 
     var dateCellTemplate = '<div class="ngCellText" ng-class="col.colIndex()"><span ng-cell-text>{{row.getProperty(col.field) | date: "' + dateFormat + '"}}</span></div>';
+    var iconCellTemplate = '<div class="ngCellText text-center" ng-class="col.colIndex()" ng-show="row.entity[col.field]"><span title="Protected" ng-cell-text class="glyphicon glyphicon-ok" /></div>';
 
     $scope.gridOptions = {
         data: 'deletedDocuments',
-        selectedItems: $scope.selectedDocs, 
-        keepLastSelected: false, 
+        selectedItems: $scope.selectedDocs,
+        enableColumnResize: true,
+        //keepLastSelected: false,        
+        showFooter: true,
+        //showFilter: true,
+        filterOptions: {
+            filterText: $scope.filterText
+        },
+        //enablePaging: true,
+        //pagingOptions: $scope.pagingOptions,
         //rowTemplate: '<div style="height: 100%" ng-class="{green: row.getProperty(\'IsProtected\') == true}">' + 
         //                '<div ng-style="{ \'cursor\': row.cursor }" ng-repeat="col in renderedColumns" ng-class="col.colIndex()" class="ngCell ">' +
         //                   '<div class="ngVerticalBar" ng-style="{height: rowHeight}" ng-class="{ ngVerticalBarVisible: !$last }"> </div>' +
@@ -105,9 +138,9 @@
                      { field: 'ArchivedBy', displayName: 'Archived By' },
                      { field: 'DeletedDate', displayName: 'Deleted Date', cellTemplate: dateCellTemplate },
                      { field: 'DeletedBy', displayName: 'Deleted By' },
-                     { field: 'IsProtected', displayName: 'Protected', width: 80, resizable: true, cellTemplate: '<div class="ngCellText text-center" ng-class="col.colIndex()" ng-show="row.entity[col.field]"><span ng-cell-text class="glyphicon glyphicon-ok" /></div>' },
-                     { field: 'ProtectionStartDate', displayName: 'Protection Start', cellTemplate: dateCellTemplate },
+                     { field: 'IsProtected', displayName: 'Protected', width: '6%', cellTemplate: iconCellTemplate },
+                     { field: 'ProtectionStartDate', displayName: 'Protection Period', cellTemplate: dateCellTemplate },
                      { field: 'ProtectionEndDate', displayName: 'Protection End', cellTemplate: dateCellTemplate }]
-    };    
+    };
 
 });
